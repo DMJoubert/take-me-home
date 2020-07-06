3b52246567a7c6917398e30e2ca8c1b94dd626e7
Devin Joubert <djoubert@sportingindex.com>
2020-07-06 11:30:12 +0200

    Optimised import

--- src/main/java/com/carbon/plugins/beammeup/commands/HomeCommand.java
+++ src/main/java/com/carbon/plugins/beammeup/commands/HomeCommand.java
@@ -1,6 +1,5 @@
 package com.carbon.plugins.beammeup.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
